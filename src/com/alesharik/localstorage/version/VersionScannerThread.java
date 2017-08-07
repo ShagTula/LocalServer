a55953341e47e32ee5c534a10bb231772eab4d1a
alesharik <alesharikReserv@yandex.ru>
2017-08-08 01:48:04 +0300

    Revert "Updated ignore file"
    This reverts commit af865a5

--- src/com/alesharik/localstorage/version/VersionScannerThread.java
+++
@@ -1,7 +0,0 @@
-package com.alesharik.localstorage.version;
-
-/**
- * This thread scans version folder for new versions and update {@link VersionList}
- */
-final class VersionScannerThread extends Thread {
-}
